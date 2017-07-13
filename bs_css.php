ef8085ea0fcb285ffce4076b125e1e2938eb5640
jafrax <jafraxx@yahoo.co.id>
2017-07-13 12:09:33 +0700

    update

--- bs_css.php
+++ bs_css.php
@@ -1,12 +1,10 @@
 <!-- Le styles -->
-<link href="<?php echo $doc_root_css;?>bootstrap.css" rel="stylesheet">
-<link href="<?php echo $doc_root_css;?>bootstrap-responsive.css" rel="stylesheet">
-<link href="<?php echo $doc_root_css;?>custom.css" rel="stylesheet">
-<link href="<?php echo $doc_root_css;?>menu_nav.css" rel="stylesheet" type="text/css">
-
+<link href="<?php echo $addr_root_css;?>bootstrap.css" rel="stylesheet">
+<link href="<?php echo $addr_root_css;?>bootstrap-responsive.css" rel="stylesheet">
+<link href="<?php echo $addr_root_css;?>custom.css" rel="stylesheet">
+<link href="<?php echo $addr_root_css;?>menu_nav.css" rel="stylesheet" type="text/css">
 <link href="<?php echo $addr_root_spry;?>SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php echo $doc_root_css;?>datepicker/jquery.ui.all.css" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php echo $doc_root_css;?>jquery-ui-1.8.10.custom.css" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo $addr_root_css;?>datepicker/jquery.ui.all.css" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo $addr_root_css;?>jquery-ui-1.8.10.custom.css" />
 <link rel="stylesheet" href="<?php echo $addr_root_validation_engine;?>validationEngine.jquery.css" />
 
-   
